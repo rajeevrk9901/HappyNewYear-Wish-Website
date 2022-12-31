c36e5b025ff8db4f5848bb892158eb600781bd52
rajeevrk9901 <rajeevrk9901@gmail.com>
2022-12-31 12:52:58 +0530

    Created basic wish Website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+import * as React from 'react';
+import { Routes, Route, useParams } from 'react-router-dom';
+import Hny from './Hny';
+
 
 function App() {
+
+  function ProfilePage() {
+    // Get the userId param from the URL.
+    let { userId } = useParams();
+
+    return (
+      <>
+        <div className="text uppercase drop-shadow-2xl">
+          {userId}
+        </div>
+        <Hny />
+      </>
+    );
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex justify-center items-center min-h-screen">
+      <div className='flex flex-col justify-center items-center'>
+
+        <Routes>
+          <Route exact path="/" element={<Hny />} />
+          <Route path=":userId" element={<ProfilePage />} />
+          {/* <div class="text">amit</div> */}
+        </Routes>
+      </div>
     </div>
   );
 }
